ee0f53930792356c970d61de41067cdf25d89eef
Furious <wmstudt25@gmail.com>
2015-05-20 09:56:22 -0600

    Create chat.php

---
+++ chat.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>PHP Chat?</title>
+		<?php
+		$_POST = 
+		?>
+		<form method="post" action="<?php echo $_POST?>">
+			<input type="text" name="value">
+			<input type="submit">
+		</form>
+	</head>
